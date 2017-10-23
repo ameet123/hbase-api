d778322c1061b248286af5d9a8644f70317d3d39
ameet.chaubal <ameet.chaubal@accenture.com>
2017-10-23 14:05:55 -0400

    command line parse

--- src/main/java/com/anthem/iidr/hbase/IIDRHbaseApp.java
+++ src/main/java/com/anthem/iidr/hbase/IIDRHbaseApp.java
@@ -1,5 +1,8 @@
 package com.anthem.iidr.hbase;
 
+import com.google.common.base.Stopwatch;
+import com.google.common.base.Strings;
+import org.apache.commons.cli.*;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.TableName;
@@ -17,21 +20,23 @@ import java.util.Random;
 
 /**
  * Created by ameet.chaubal on 10/23/2017.
+ * IIDR sample hbase insert
  */
 public class IIDRHbaseApp {
     private static final Logger LOGGER = LoggerFactory.getLogger(IIDRHbaseApp.class);
+    private static final String HBASE_SITE = "/etc/alternatives/hbase-conf/hbase-site.xml";
+    private static final String PROGRAM_NAME = "iidrAnthem";
+    private static final String TABLE_NAME = "dv_bdfrawz:ameet-tab";
+    private static final String COLUMN_FAMILY_NAME = "cf1";
+    private static final String COLUMN_NAME = "col1";
+    private static final String PRINCIPAL = "AF55267@DEVAD.WELLPOINT.COM";
+    private static String KEYTAB;
     private Configuration conf;
-    private Connection connection;
-    private Table table;
-    private String TABLE_NAME;
     private Random rand;
-    private byte[] COLUMN_FAMILY_NAME;
-    private byte[] COLUMN_NAME;
-    private String PRINCIPAL = "AF55267@DEVAD.WELLPOINT.COM";
 
     public IIDRHbaseApp(String tableName, String columnFamily, String column) {
         conf = HBaseConfiguration.create();
-        conf.addResource("/etc/alternatives/hbase-conf/hbase-site.xml");
+        conf.addResource(HBASE_SITE);
         conf.set("hadoop.security.authentication", "kerberos");
         conf.set("hbase.security.authentication", "kerberos");
 
@@ -39,55 +44,68 @@ public class IIDRHbaseApp {
 
         UserGroupInformation.setConfiguration(conf);
         try {
-            UserGroupInformation.loginUserFromKeytab(PRINCIPAL, "/home/af55267/apps/hbase-api/af55267.keytab");
+            UserGroupInformation.loginUserFromKeytab(PRINCIPAL, KEYTAB);
         } catch (IOException e) {
-            e.printStackTrace();
+            LOGGER.error("Error logging to Hadoop using kerberos. exit");
+            System.exit(4);
         }
-        TABLE_NAME = tableName;
-        COLUMN_FAMILY_NAME = columnFamily.getBytes();
-        COLUMN_NAME = column.getBytes();
-        rand = new Random();
-        System.out.println("Table:" + tableName);
         LOGGER.info("table:{} cf:{} col:{}", TABLE_NAME, COLUMN_FAMILY_NAME, COLUMN_NAME);
+        rand = new Random();
     }
 
     public static void main(String[] args) {
-        System.out.println("hbase insert anthem---");
+        parseOptions(args);
+        if (args == null || args.length == 0) {
+            LOGGER.error("No arguments passed");
+            System.exit(1);
+        }
+        String keytab = args[0];
+        if (Strings.isNullOrEmpty(keytab)) {
+            LOGGER.error("Err: first argument needs to be keytab file absolute path");
+            System.exit(2);
+        }
+        LOGGER.info("Start: hbase insertion anthem. keytab:{}", keytab);
+        System.exit(3);
 
-        IIDRHbaseApp iidrHbaseApp = new IIDRHbaseApp("dv_bdfrawz:ameet-tab", "cf1", "col1");
-        System.out.println("Instantiation done....");
+        IIDRHbaseApp iidrHbaseApp = new IIDRHbaseApp(TABLE_NAME, COLUMN_FAMILY_NAME, COLUMN_NAME);
+        LOGGER.info(">>Instantiation done....");
         iidrHbaseApp.insertData();
     }
 
+    private static void parseOptions(String[] args) {
+        Options options = new Options();
+        Option keytab = new Option("k", "keytab", true, "keytab absolute file path");
+        keytab.setRequired(true);
+        options.addOption(keytab);
+        CommandLineParser parser = new BasicParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd;
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            LOGGER.error(e.getMessage());
+            formatter.printHelp(PROGRAM_NAME, options);
+            System.exit(1);
+            return;
+        }
+        KEYTAB = cmd.getOptionValue("keytab");
+        LOGGER.debug(">>Parsed keytab:{}", KEYTAB);
+    }
+
     public void insertData() {
+        Stopwatch stopwatch = Stopwatch.createStarted();
         try (Connection connection = ConnectionFactory.createConnection(conf)) {
             System.out.println("Connection ready...");
             String rowKey = "myKey-" + rand.nextInt(100000);
             String val = "hello world-" + rand.nextInt(934552);
             Put put = new Put(Bytes.toBytes(rowKey));
-            put.addColumn(COLUMN_FAMILY_NAME, COLUMN_NAME, Bytes.toBytes(val));
-            table = connection.getTable(TableName.valueOf(TABLE_NAME));
-            System.out.println("Table info from object:" + table.getName());
+            put.addColumn(COLUMN_FAMILY_NAME.getBytes(), COLUMN_NAME.getBytes(), Bytes.toBytes(val));
+            Table table = connection.getTable(TableName.valueOf(TABLE_NAME));
+            LOGGER.debug(">>Table info from object:" + table.getName());
             table.put(put);
         } catch (IOException e) {
-            e.printStackTrace();
-            LOGGER.error("Error connecting to hbase, no insert done.");
+            LOGGER.error("Error connecting to hbase, no insert done.", e);
         }
-    }
-
-    public Configuration getConf() {
-        return conf;
-    }
-
-    public void setConf(Configuration conf) {
-        this.conf = conf;
-    }
-
-    public Connection getConnection() {
-        return connection;
-    }
-
-    public void setConnection(Connection connection) {
-        this.connection = connection;
+        LOGGER.debug(">>Insert completed in:{}", stopwatch.stop());
     }
 }
